bc53872acca683ba5df5b12f4cfe80e94da9fe36
dileepshukla901 <dileepshukla901@gmail.com>
2020-05-24 13:49:27 +0530

    initial commint

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Dileep Shukla: I have made a dynamic updated application 
+          Edit <code>src/App.js</code> and save to reload.
+          hey there !!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
